05122eadebbeba6f71ea4f874ddbd0d97e30141e
Mauro Famà <MAUROFAMA99@GMAIL.COM>
2025-07-28 18:39:54 +0200

    updated higgs configuration

--- code/benchmark/generate_config.py
+++ code/benchmark/generate_config.py
@@ -17,16 +17,16 @@ def generate_config_files(datasets, algorithms, window_slide_pairs, query_label_
                                 f"min_size={min_size}\n"
                             )
                             config_filename = f"config_a{algorithm}_S{size}_s{slide}_q{query_type}_M{max_size}_m{min_size}.txt"
-                            config_filepath = os.path.join("config/ldbc", config_filename)
+                            config_filepath = os.path.join("config/higgs", config_filename)
                             with open(config_filepath, 'w') as config_file:
                                 config_file.write(config_content)
                             print(f"Generated {config_filepath}")
 
 def main():
-    datasets = ["code/dataset/ldbc/social-graph12_14v4_bursted.txt"]
+    datasets = ["code/dataset/higgs-activity/higgs-activity_time_postprocess.txt"]
     algorithms = [1,0] # 1 adaptive, 0 sliding window
-    window_slide_pairs = [(216000, 21600, 237600, 108000), (172800, 17280, 189880, 86400), (129600, 12960, 142560, 64800), (86400, 8640, 95040, 43200)]
-    query_label_pairs = [(4, [3,0,8]), (1, [0]), (5, [3,0,8]), (2, [3,0]), (10, [9,3,0]), (7, [8,9,0])]
+    window_slide_pairs = [(4000, 400, 4400, 2400), (3200, 320, 3520, 1920), (2400, 240, 2640, 1440), (1600, 160, 1160, 960)]
+    query_label_pairs = [(1,[1]), (5,[2,1,3]), (7,[2,3,1]), (2,[2,1]), (10,[2,3,1]), (6,[2,1]), (3,[3,2,1]), (4,[2,1,3])]
 
     # 21600, 43200, 64800, 86400, 108000, 194400, 259200, 324000, 388800, 453600, 518400, 583200, 648000, 712800, 777600, 842800, 907200, 972000, 1036800, 1108800, 1180800, 1252800, 1324800, 1396800, 1468800, 1540800, 1612800, 1684800, 1756800, 1828800, 1900800, 1972800
 
@@ -53,7 +53,7 @@ def main():
     # ldbc:
     # [(4, [3,0,8]), (1, [0]), (5, [3,0,8]), (2, [3,0]), (10, [9,3,0]), (7, [8,9,0])]
     # datasets = ["code/dataset/ldbc/social-graph12_14v4_bursted.txt"]
-    # [(216000, 21600, 324000, 108000), (172800, 17280, 259200, 86400), (129600, 12960, 194400, 64800), (86400, 8640, 129600, 43200)]
+    # window_slide_pairs = [(216000, 21600, 237600, 108000), (172800, 17280, 189880, 86400), (129600, 12960, 142560, 64800), (86400, 8640, 95040, 43200)]
 
     generate_config_files(datasets, algorithms, window_slide_pairs, query_label_pairs)
 
